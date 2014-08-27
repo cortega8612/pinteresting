66ee29875b517212b3685d75b1e79ecebdb76469
cortega8612 <carlos.ortega86@gmail.com>
2014-08-27 12:06:30 -0500

    implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Note to set this to your actual host
+  #Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://carlos-pinteresting.herokuapp.com' }
 end
